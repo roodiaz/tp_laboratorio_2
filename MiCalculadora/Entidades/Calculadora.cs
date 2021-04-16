3b32be014c66aac86f96947b41c7b06da35127af
Rocio Diaz <roabigail.diaz@gmail.com>
2021-04-16 16:29:03 -0300

    clase numero terminada

---
+++ MiCalculadora/Entidades/Calculadora.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    static class Calculadora
+    {
+        /// <summary>
+        /// Valida que sea un operador valido, caso contrario asigna "+" por defecto
+        /// </summary>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        private static string ValidarOperador(string operador)
+        {
+            if (string.IsNullOrEmpty(operador))
+                operador = "+";
+
+            if (operador != "+" && operador != "-" && operador != "/" && operador != "*")
+                operador = "+";
+
+            return operador;
+        }
+
+        /// <summary>
+        /// Valida el operar y realiza las operaciones entre numeros
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            double resultado = 0;
+
+            switch (ValidarOperador(operador))
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+            }
+            return resultado;
+        }
+    }
+}
